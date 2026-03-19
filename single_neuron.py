e0fa8a3a52791d6384028e5b1e307e9032feb763
Arslan Rejebov <arslanrejepov174@gmail.com>
2026-03-19 11:04:18 +0800

    simple neuron build

--- single_neuron.py
+++ single_neuron.py
@@ -1,16 +1,40 @@
 import numpy as np
 
-def weighted_sum(inputs, weights, bias):
-    inputs  = np.array(inputs,  dtype=float)
-    weights = np.array(weights, dtype=float)
+x = np.array([1.0]) 
+y_true = np.array([1.0])
 
-    z = np.dot(inputs, weights) + bias
-    return z
+w=np.random.randn()
+b=np.random.randn()
 
-def step_activation(z):
-    return 1 if z >= 0 else 0
+#activation set sigmoid
+def sigmoid(z):
+    return 1/(1+np.exp(-z))
 
-def neuron(inputs, weights, bias, activation_fn=step_activation):
-    z = weighted_sum(inputs, weights, bias)
-    output = activation_fn
-    return output
+def forward(x,w,b):
+    z=w*x+b
+    pred=sigmoid(z)
+    return pred
+
+def loss(y_pred,y_true):
+    return (y_pred-y_true)**2
+
+lr=0.01
+
+for epoch in range(100):
+    
+    y_pred = forward(x,w,b)
+    l = loss(y_pred,y_true)
+
+    dL_dy = 2*(y_pred-y_true)
+    dy_dz = y_pred * (1-y_pred)
+
+    dL_dw = dL_dy *dy_dz * x
+
+    dL_db = dL_dy * dy_dz * 1
+
+    w -= lr * dL_dw
+    b -= lr * dL_db
+
+    if epoch % 10 == 0:
+        print(f"Epoch {epoch}: Loss = {l[0]:.4f}")
+    
